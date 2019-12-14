73a261294098ef0f8abf35ad054ad316fd3219a5
Spronjee <i.chukhlebov6530@gmail.com>
2019-12-15 01:17:43 +0200

    added crypto mod

---
+++ environment/crypto.js
@@ -0,0 +1,14 @@
+const NodeRSA = require('node-rsa');
+
+const key = new NodeRSA({b:512});
+
+const encode = (data) => {
+    return key.encrypt(data, 'base64');
+}
+
+const decode = (data) => {
+    return key.decrypted(data, 'utf-8');
+}
+
+module.exports.encode() = encode;
+module.exports.decode() = decode;
