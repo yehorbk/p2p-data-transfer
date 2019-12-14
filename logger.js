2820cdacf3b8b5034b2cebea0f655732dfed91d5
Spronjee <i.chukhlebov6530@gmail.com>
2019-12-14 23:19:43 +0200

    file loggin fuctionality added

--- logger.js
+++ logger.js
@@ -1,3 +1,15 @@
-const log = (data) => console.log(data);
+const fs = require('fs'); 
+
+const log = (data) => {
+    console.log(data);
+    logToFile(data);
+}
+
+const logToFile = (data) => {
+    fs.appendFile('./logs/server.log', data, (error) => {
+        throw new Error(error);
+    })
+
+}
 
 module.exports.log = log;
